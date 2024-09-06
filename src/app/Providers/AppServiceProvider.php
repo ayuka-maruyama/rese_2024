4a9dcfee3ba570791c46e16f2fc6ef0aa78418a8
ayuka <basketball110512@gmail.com>
2024-09-07 07:50:10 +0900

    メール認証関連記述の修正

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+            return (new MailMessage)
+                ->subject('メールアドレスの確認')
+                ->line('ボタンを押してメールアドレスの確認をしてください！')
+                ->action('メールアドレス確認', $url);
+        });
     }
 }
