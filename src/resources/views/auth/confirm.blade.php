b5bd471e7fda0a24a62650ece114f9f7c1f5653b
ayuka <basketball110512@gmail.com>
2024-09-07 07:51:00 +0900

    会員登録後画面の修正

--- src/resources/views/auth/confirm.blade.php
+++ src/resources/views/auth/confirm.blade.php
@@ -8,7 +8,6 @@
 <div class="confirm">
     <div class="confirm-form">
         <h2 class="confirm-txt">会員登録ありがとうございます</h2>
-        <p class="confirm-msg">ご登録いただいたメールアドレスにメールを送りましたので、認証をお願いします</p>
         <form class="confirm__form-area" action="/login" method="get">
             @csrf
             <button class="confirm__form-btn" type="submit">
