afb8bf1257bdf59521c0919a489f2c8f6dabeef0
ayuka <basketball110512@gmail.com>
2024-09-07 07:48:48 +0900

    新規会員登録コントローラー、リクエスト作成

---
+++ src/app/Http/Controllers/RegisterController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Events\Registered;
+
+class RegisterController extends Controller
+{
+    public function open()
+    {
+        return view('auth/register');
+    }
+
+    public function store(RegisterRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        // event(new Registered($user));
+
+        // return redirect('/email/verify');
+        return view('auth/confirm');
+    }
+}
