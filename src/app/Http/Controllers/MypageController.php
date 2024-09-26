3c2ba3eab4b3923ed1e4217a8892ac2a55beafd0
ayuka <basketball110512@gmail.com>
2024-09-26 20:02:10 +0900

    マイページの作成、使用画像の追加

--- src/app/Http/Controllers/MypageController.php
+++ src/app/Http/Controllers/MypageController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Shop;
+use App\Models\Area;
+use App\Models\Genre;
+use App\Models\Favorite;
+use App\Models\Reservation;
 
 class MypageController extends Controller
 {
     public function index()
     {
-        return view('mypage');
+        $user = Auth::user();
+
+        // お気に入りの店舗IDリストを配列で取得
+        $favoriteShopIds = $user ? Favorite::where('user_id', $user->id)->pluck('shop_id')->toArray() : [];
+
+        // お気に入りの店舗情報を取得
+        $favoriteShops = Shop::whereIn('id', $favoriteShopIds)->with('area', 'genre')->get();
+
+        // 予約情報の店舗IDを取得
+        $reservations = $user ? Reservation::where('user_id', $user->id)->with('shop')->get() : [];
+
+        return view('mypage', compact('user', 'favoriteShops', 'favoriteShopIds', 'reservations'));
     }
 }
--- src/resources/views/mypage.blade.php
+++ src/resources/views/mypage.blade.php
@@ -1,9 +1,86 @@
 @extends('layouts.app')
 
 @section('css')
-
+<link rel="stylesheet" href="{{ asset('css/shop.css') }}">
+<link rel="stylesheet" href="{{ asset('css/mypage.css') }}">
 @endsection
 
 @section('content')
+<div class="mypage">
+    <div class="login-user">
+        <h2 class="user-name">{{ $user->name }} さん</h2>
+    </div>
+    <div class="mypage-content">
+        <div class="reserve">
+            <h3 class="reserve-ttl">予約状況</h3>
+            @foreach($reservations as $reservation)
+            <div class="reserve-card">
+                <form action="" method="post">
+                    @csrf
+                    <div class="card-header">
+                        <img class="clock" src="{{ asset('img/clock.svg') }}" alt="clock">
+                        <div class="justify">
+                            <span class="reserve-number">予約{{ $loop->iteration }}</span>
+                            <button class="delete">
+                                <img class="close" src="{{ asset('img/close.svg') }}" alt="close">
+                            </button>
+                        </div>
+                    </div>
+                    <div class="reserve-details">
+                        <table class="reserve-table">
+                            <tr>
+                                <th class="table-header">Shop</th>
+                                <td class="table-date">{{ $reservation->shop->shop_name }}</td>
+                            </tr>
+                            <tr>
+                                <th class="table-header">Date</th>
+                                <td class="table-date">{{ $reservation->date }}</td>
+                            </tr>
+                            <tr>
+                                <th class="table-header">Time</th>
+                                <td class="table-date">{{ $reservation->time }}</td>
+                            </tr>
+                            <tr>
+                                <th class="table-header">Number</th>
+                                <td class="table-date">{{ $reservation->number_gest }}</td>
+                            </tr>
+                        </table>
+                    </div>
+                </form>
+            </div>
+            @endforeach
+        </div>
+        <div class="favorite">
+            <h3 class="favorite-ttl">お気に入り店舗</h3>
+            <div class="shop-card__wrap">
+                @foreach($favoriteShops as $shop)
+                <div class="shop-card">
+                    <form class="shop-card__form" action="/detail/{{ $shop->id }}" method="get">
+                        @csrf
+                        <div class="shop-card__img">
+                            <img class="shop-card__img" src="{{ $shop->image }}" alt="{{ $shop->shop_name }}">
+                        </div>
+                        <div class="shop-card__txt">
+                            <h3 class="shop-card__shop-name">{{ $shop->shop_name }}</h3>
+                            <div class="shop-card__txt-flex">
+                                <p class="shop-card__area">#{{ $shop->area->area_name }}</p>
+                                <p class="shop-card_genre">#{{ $shop->genre->genre_name }}</p>
+                            </div>
+                            <div class="shop-card__btn">
+                                <!-- 詳しくみるボタン -->
+                                <button class="btn-submit" type="submit">詳しくみる</button>
 
+                                <!-- お気に入りボタン -->
+                                <button class="favorite-btn {{ in_array($shop->id, $favoriteShopIds) ? 'favorited' : '' }}" data-id="{{ $shop->id }}">
+                                    ❤
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
