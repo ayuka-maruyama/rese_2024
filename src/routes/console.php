2d2c51a71e82a30c95b3b3bd4235b84e1f581cd9
ayuka <basketball110512@gmail.com>
2024-10-21 05:43:55 +0900

    タスクスケジューラー自動実行(ローカル環境)の動作確認

--- src/routes/console.php
+++ src/routes/console.php
@@ -1,8 +1,13 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
+// use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use App\Console\Commands\SendMailCommand;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+// Artisan::command('inspire', function () {
+//     $this->comment(Inspiring::quote());
+// })->purpose('Display an inspiring quote')->hourly();
+
+// 毎日朝9時にcommandファイルで定義した内容を実行する
+Schedule::command('app:send-mail-command')->dailyAt('09:00');
