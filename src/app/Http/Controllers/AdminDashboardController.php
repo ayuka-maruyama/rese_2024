1a4b582d7af0ede2e82e04b2e33c0bf5dc35dd60
ayuka <basketball110512@gmail.com>
2024-10-29 04:31:38 +0900

    AdminDashboardで実装されるowner新規登録画面表示及び検索機能のコントローラー作成

--- src/app/Http/Controllers/AdminDashboardController.php
+++ src/app/Http/Controllers/AdminDashboardController.php
@@ -3,12 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Shop;
+
+
 
 class AdminDashboardController extends Controller
 {
-    public function openAdminDashboard(){
-        return view('admin/admin-dashboard');
+    public function openAdminDashboard()
+    {
+        $user = Auth::user();
+        $shops = Shop::with('area', 'genre', 'user')->paginate(5);
+
+        return view('admin/admin-dashboard', compact('user', 'shops'));
     }
 
-    
+    public function openOwnerCreate()
+    {
+        return view('admin/admin-register');
+    }
+
+    public function search(Request $request)
+    {
+        $user = Auth::user();
+        $search = $request->input('search');
+        $shops = Shop::with('user')
+            ->when($search, function ($query, $search) {
+                $query->where('shop_name', 'like', '%' . $search . '%')
+                    ->orWhereHas('user', function ($q) use ($search) {
+                        $q->where('name', 'like', '%' . $search . '%');
+                    });
+            })
+            ->paginate(10);
+
+        return view('admin.admin-dashboard', compact('user', 'shops', 'search'));
+    }
 }
