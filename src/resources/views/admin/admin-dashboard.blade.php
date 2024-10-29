32ea3cc2f4c9a3e0b76357ae1df2702e7bda3f49
ayuka <basketball110512@gmail.com>
2024-10-29 20:38:05 +0900

    店舗、店舗代表者一覧表示部分の追加

--- src/resources/views/admin/admin-dashboard.blade.php
+++ src/resources/views/admin/admin-dashboard.blade.php
@@ -17,28 +17,28 @@
         <a href="{{ route('admin.owner-create') }}" class="create-shop">店舗管理者登録</a>
     </div>
     <div class="owner">
-        <form action="{{ route('admin.shop-detail') }}" method="post">
-            @csrf
-            <table class="owner__table">
-                <tr class="table-row">
-                    <th class="table-header">店舗名</th>
-                    <th class="table-header">店舗代表者</th>
-                </tr>
-                @forelse($shops as $shop)
-                <tr class="table-row">
-                    <td class="table-data">{{ $shop->shop_name }}</td>
-                    <td class="table-data">{{ $shop->user->name ?? '代表者なし' }}</td>
-                    <td class="table-data">
-                        <button class="detail-btn" type="submit">詳細</button>
-                    </td>
-                </tr>
-                @empty
-                <tr>
-                    <td colspan="3">検索結果が見つかりませんでした。</td>
-                </tr>
-                @endforelse
-            </table>
-        </form>
+        <!-- <form action="" method="post">
+            @csrf -->
+        <table class="owner__table">
+            <tr class="table-row">
+                <th class="table-header">店舗名</th>
+                <th class="table-header">店舗代表者</th>
+            </tr>
+            @forelse($shops as $shop)
+            <tr class="table-row">
+                <td class="table-data">{{ $shop->shop_name }}</td>
+                <td class="table-data">{{ $shop->user->name ?? '代表者なし' }}</td>
+                <td class="table-data">
+                    <button class="detail-btn" type="submit">詳細</button>
+                </td>
+            </tr>
+            @empty
+            <tr>
+                <td colspan="3">検索結果が見つかりませんでした。</td>
+            </tr>
+            @endforelse
+        </table>
+        <!-- </form> -->
         <div class="pagination">
             {{ $shops->links('vendor.pagination.default') }}
         </div>
