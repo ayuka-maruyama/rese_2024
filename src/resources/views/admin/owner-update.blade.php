114553ecb5b60d034fbf773da4eefeb9a8ad5f7f
ayuka <basketball110512@gmail.com>
2024-10-31 05:10:10 +0900

    ownerの更新機能実装によるビュー、cssの修正

--- src/resources/views/admin/owner-update.blade.php
+++ src/resources/views/admin/owner-update.blade.php
@@ -1,49 +1,50 @@
 @extends('layouts.app')
 
 @section('css')
+<link rel="stylesheet" href="{{ asset('css/owner-update.css') }}">
 @endsection
 
 @section('content')
-<form action="{{ route('admin.owner-update', ['id' => $request->id]) }}" method="post">
-    @csrf
-    <table>
-        <tr class="table-row">
-            <th class="table-header">店舗管理者名</th>
-            <td class="table-date">
-                <input type="text" name="name" value="{{ $request->name }}">
-            </td>
-        </tr>
-        <tr class="table-row">
-            <th class="table-header">登録済みメールアドレス</th>
-            <td class="table-date">
-                <input type="text" name="email" value="{{ $request->email }}">
-            </td>
-        </tr>
-        <tr class="table-row">
-            <th class="table-header">パスワード</th>
-            <td class="table-date">
-                @if($request->password)
-                <span><input type="text" name="password" value=""></span>
-                @endif
-            </td>
-        </tr>
-        <tr class="table-row">
-            <th class="table-header"></th>
-            <td class="table-date">
-                <button class="update-btn" type="submit">更新</button>
-            </td>
-        </tr>
-    </table>
-</form>
-
-@error('name')
-<P>{{ $message }}</P>
-@enderror
-@error('email')
-<P>{{ $message }}</P>
-@enderror
-@error('password')
-<P>{{ $message }}</P>
-@enderror
-
+<div class="update-area">
+    <form action="{{ route('admin.owner-update', ['id' => $request->id]) }}" method="post">
+        @csrf
+        <table>
+            <tr class="table-row">
+                <th class="table-header">店舗管理者名</th>
+                <td class="table-date">
+                    <input class="input-area" type="text" name="name" value="{{ $request->name }}">
+                </td>
+            </tr>
+            <tr class="table-row">
+                <th class="table-header">メールアドレス</th>
+                <td class="table-date">
+                    <input class="input-area" type="text" name="email" value="{{ $request->email }}">
+                </td>
+            </tr>
+            <tr class="table-row">
+                <th class="table-header">パスワード</th>
+                <td class="table-date">
+                    @if($request->password)
+                    <input class="input-area" type="text" name="password" value="">
+                    @endif
+                </td>
+            </tr>
+            <tr class="table-row">
+                <th class="table-header"></th>
+                <td class="table-date__btn">
+                    <button class="update-btn" type="submit">更新</button>
+                </td>
+            </tr>
+        </table>
+    </form>
+    @if ($errors->any())
+    <div class="error-index">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+</div>
 @endsection
