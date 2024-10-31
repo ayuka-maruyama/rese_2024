e84c8b665ef1bf436ca528cf71e4afe65139dd63
ayuka <basketball110512@gmail.com>
2024-11-01 08:08:16 +0900

    メール内容入力画面、メール内容のbladeファイルおよびcssの作成

---
+++ src/resources/views/admin/email.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/reserve.css') }}">
+<link rel="stylesheet" href="{{ asset('css/admin-email.css') }}">
+@endsection
+
+@section('content')
+<div class="back">
+    <form class="shop-detail__form" action="{{ route('admin.dashboard') }}" method="get">
+        <button class="back-btn" type="submit">&lt;</button>
+        <span class="message">戻る</span>
+    </form>
+</div>
+<div class="mail-input_area">
+    <form action="{{ route('admin.sendEmail') }}" method="POST">
+        @csrf
+        <input type="hidden" name="user_id" value="{{ $owner->id }}">
+        <div class="mail-content">
+            <table class="table-area">
+                <tr class="table-row">
+                    <th class="table-header">送信先</th>
+                    <td class="table-data mail">{{ $owner->email }}</td>
+                </tr>
+                <tr class="table-row">
+                    <th class="table-header">件名</th>
+                    <td class="table-data">
+                        <input type="text" name="subject" id="subject" required>
+                    </td>
+                </tr>
+                <tr class="table-row">
+                    <th class="table-header">本文</th>
+                    <td class="table-data">
+                        <textarea name="body" id="body" required></textarea>
+                    </td>
+                </tr>
+            </table>
+            <div class="btn-area">
+                <button class="btn" type="submit">メールを送信</button>
+            </div>
+        </div>
+    </form>
+</div>
+@endsection
---
+++ src/resources/views/emails/send-mail.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>{{ $subjectLine }}</title>
+</head>
+
+<body>
+    <p>{{ $user->name }} 様</p>
+    <p>{{ $bodyContent }}</p>
+    <br>
+    <p>
+        ーーーーーーーーーーーーーーーー<br>
+        　　　Rese 管理者<br>
+        ーーーーーーーーーーーーーーーー
+    </p>
+</body>
+
+</html>
