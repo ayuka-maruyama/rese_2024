5d469137b062bb99e45d1ad3bd848df84e4ee965
ayuka <basketball110512@gmail.com>
2024-11-02 08:50:10 +0900

    店舗新規作成画面の作成

---
+++ src/resources/views/owner/shop-register.blade.php
@@ -0,0 +1,108 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/shop-register.css') }}">
+@endsection
+
+@section('content')
+<div class="register-area">
+    <h3 class="title">店舗新規登録</h3>
+    <form action="{{ route('owner.shop-create') }}" method="post" enctype="multipart/form-data">
+        @csrf
+        <div class="input-area">
+            <div class="ttl">
+                <label for="shop_name" class="label-name">
+                    店舗名<span class="required">必須</span>
+                </label>
+            </div>
+            <input class="input-txt" type="text" name="shop_name" id="shop_name" placeholder="店舗名を入力してください。">
+        </div>
+        <div class="flex">
+            <div class="input-area flex-area">
+                <div class="ttl">
+                    <label for="area_id" class="label-name">
+                        エリア<span class="required">必須</span>
+                    </label>
+                </div>
+                <select class="pulldown" name="area_id" id="area_id">
+                    @foreach($areas as $area)
+                    <option class="list" value="{{ $area->id }}">{{ $area->area_name }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="input-area flex-area">
+                <div class="ttl">
+                    <label for="genre" class="label-name">
+                        ジャンル<span class="required">必須</span>
+                    </label>
+                </div>
+                <select class="pulldown" name="genre_id" id="genre_id">
+                    @foreach($genres as $genre)
+                    <option class="list" value="{{ $genre->id }}">{{ $genre->genre_name }}</option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+        <div class="input-area">
+            <div class="ttl">
+                <label for="summary" class="label-name">
+                    店舗概要<span class="required">必須</span>
+                </label>
+            </div>
+            <textarea class="textarea" name="summary" id="summary" placeholder="店舗概要を入力してください。"></textarea>
+        </div>
+        <div class="input-area">
+            <div class="ttl">
+                <label for="image" class="label-name">
+                    店舗画像<span class="required">必須</span>
+                </label>
+            </div>
+            <label for="image" class="file-upload">
+                画像を選択
+            </label>
+            <input type="file" name="image" id="image" style="display: none;">
+            <div id="preview-area">
+                <img id="image-preview" src="" alt="プレビュー画像" style="display: none; max-width: 100%; height: auto;">
+            </div>
+            <span id="file-name">ファイルが選択されていません</span>
+        </div>
+        <div class="btn-area">
+            <button class="back-btn" type="submit" formaction="/owner/dashboard" formmethod="get">戻る</button>
+            <button class="create-btn" type="submit">店舗新規登録</button>
+        </div>
+    </form>
+</div>
+@endsection
+
+@section('js')
+<script>
+    // textareaの高さを自動調整する
+    const textarea = document.querySelector("textarea");
+    textarea.addEventListener("input", function() {
+        this.style.height = "auto";
+        this.style.height = (this.scrollHeight) + "px";
+    });
+
+    // 画像を選択したら表示をファイル名に変更する
+    document.getElementById("image").addEventListener("change", function() {
+        const fileName = this.files[0] ? this.files[0].name : "ファイルが選択されていません";
+        document.getElementById("file-name").textContent = fileName;
+    });
+
+    // 選択した画像をプレビューで表示する
+    document.getElementById("image").addEventListener("change", function(event) {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+
+            reader.onload = function(e) {
+                const preview = document.getElementById("image-preview");
+                preview.src = e.target.result;
+                preview.style.display = "block"; // プレビュー画像を表示
+            };
+
+            reader.readAsDataURL(file); // ファイルをデータURLとして読み込む
+        }
+    });
+</script>
+@endsection
