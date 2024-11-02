1f0726bdc279e7ee4750223b1fe2d9509beac5d6
ayuka <basketball110512@gmail.com>
2024-11-03 06:44:34 +0900

    新規店舗登録用の画像登録及びshopsテーブルへの登録のコントローラーを作成

---
+++ src/app/Http/Controllers/ShopRegisterController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\ShopRegisterRequest;
+use App\Models\User;
+use App\Models\Shop;
+use App\Models\Area;
+use App\Models\Genre;
+use Illuminate\Support\Facades\Auth;
+
+
+class ShopRegisterController extends Controller
+{
+    public function openShopRegister()
+    {
+        $user = Auth::user();
+        $areas = Area::all();
+        $genres = Genre::all();
+
+        return view('owner.shop-register', compact('user', 'areas', 'genres'));
+    }
+
+    public function createShopRegister(ShopRegisterRequest $request)
+    {
+        $userId = $request->user_id;
+
+        // 画像ファイルの保存
+        if ($request->hasFile('image')) {
+            $filePath = $request->file('image')->store('public/shop-images');
+            $fileName = basename($filePath);
+        } else {
+            $fileName = null;
+        }
+
+        $shop = Shop::create([
+            'shop_name' => $request->shop_name,
+            'area_id' => $request->area_id,
+            'genre_id' => $request->genre_id,
+            'summary' => $request->summary,
+            'image' => 'storage/shop-images/' . $fileName,
+            'user_id' => $userId, // ここでuser_idを設定
+        ]);
+
+        return view('owner.shop-created', compact('shop'));
+    }
+}
