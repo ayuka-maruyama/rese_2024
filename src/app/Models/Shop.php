5ad731fcdf91be21b1a71d510c56aaca57d99638
ayuka <basketball110512@gmail.com>
2025-02-17 19:55:50 +0900

    Pro入会テスト:Shopモデルにevaluationモデルのリレーションを追加

--- src/app/Models/Shop.php
+++ src/app/Models/Shop.php
@@ -27,4 +27,9 @@ class Shop extends Model
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+
+    public function evaluation()
+    {
+        return $this->hasOne(Evaluation::class);
+    }
 }
