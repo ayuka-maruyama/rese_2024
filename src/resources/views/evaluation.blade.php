786700f419f48308345a65bad2f7ec39aac9d75d
ayuka <basketball110512@gmail.com>
2025-02-19 01:23:02 +0900

    Pro入会テスト:口コミ投稿画面のblade,cssファイル修正

--- src/resources/views/evaluation.blade.php
+++ src/resources/views/evaluation.blade.php
@@ -15,53 +15,52 @@
         @include('partials.shop-card', ['shop' => $shop, 'favoriteShopIds' => $favoriteShopIds])
     </div>
 
-    <div class="evaluation">
-        <div class="evaluation__area">
-            <form id="evaluation-form" action="{{ route('evaluation.confirm') }}" method="post">
-                @csrf
-                <input type="hidden" name="shop_id" value="{{ $shop->id }}">
-                <table class="evaluation__table-area">
-                    <tr class="table-row__shop">
-                        <th class="table-header">店名</th>
-                        <td class="table-data">{{ $shop->shop_name }}</td>
-                    </tr>
-                    <tr class="table-row__require">
-                        <th class="table-header">
-                            評価</br>
-                            <span class="require">必須</span>
-                        </th>
-                        <td class="table-data">
-                            <div class="rating">
-                                <span class="star" data-value="1">★</span>
-                                <span class="star" data-value="2">★</span>
-                                <span class="star" data-value="3">★</span>
-                                <span class="star" data-value="4">★</span>
-                                <span class="star" data-value="5">★</span>
-                                <input type="hidden" id="rating-value" name="evaluation" value="">
-                            </div>
-                            <p class="txt">星をクリックして入力してください</p>
-                        </td>
-                    </tr>
-                    <tr class="table-row">
-                        <th class="table-header">
-                            レビュー本文</br>
-                            <span class="require">必須</span>
-                        </th>
-                        <td class="table-data">
-                            <textarea class="evaluation-txtarea" name="comment" id="comment" cols="50" rows="8" placeholder="コメントを入力してください">{{ old('comment') }}</textarea>
-                        </td>
-                    </tr>
-                </table>
-                <div class="button-area">
-                    <button class="evaluation-submit" type="submit">送信</button>
-                    <button class="back-button" type="button" onClick="history.back();">戻る</button>
+    <div class="evaluation__area">
+        <form id="evaluation-form" action="{{ route('evaluation.confirm') }}" method="post">
+            @csrf
+            <input type="hidden" name="shop_id" value="{{ $shop->id }}">
+
+            <div class="evaluation">
+                <h3 class="select-ttl">体験を評価してください</h3>
+                <div class="rating">
+                    <span class="star" data-value="1">★</span>
+                    <span class="star" data-value="2">★</span>
+                    <span class="star" data-value="3">★</span>
+                    <span class="star" data-value="4">★</span>
+                    <span class="star" data-value="5">★</span>
+                    <input type="hidden" id="rating-value" name="evaluation" value="">
                 </div>
-            </form>
-        </div>
+            </div>
+
+            <div class="comment">
+                <h3 class="input-ttl">口コミを投稿</h3>
+                <textarea class="evaluation-textarea" name="comment" id="commentInput" cols="50" rows="8" placeholder="カジュアルな夜のお出かけにおすすめのスポット">{{ old('comment') }}</textarea>
+            </div>
+
+            <div class="add_image">
+                <h3 class="input-ttl">画像の追加</h3>
+                <div class="image-upload">
+                    <label for="image" class="file-upload">
+                        クリックして写真を追加<br>
+                        またはドラッグアンドドロップ
+                    </label>
+                    <input type="file" name="image" id="image" style="display: none;">
+                    <p id="file-name" class="file-name"></p>
+                    <div id="preview-area" class="preview-area">
+                        <img id="image-preview" class="image-preview" src="" alt="プレビュー画像" style="display: none; max-width: 100%; height: auto;">
+                    </div>
+                </div>
+            </div>
+        </form>
     </div>
 </div>
+<div class="button-area">
+    <button class="evaluation-submit" type="submit">口コミを投稿</button>
+</div>
+
 @endsection
 
 @section('js')
 <script src="{{ asset('js/evaluation.js') }}" defer></script>
+<script src="{{ asset('js/form-interactions.js') }}" defer></script>
 @endsection
