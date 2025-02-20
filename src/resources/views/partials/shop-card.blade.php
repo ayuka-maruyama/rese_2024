424ecbd5b0bf82862fb2d03ceaeccfd42ce3a930
ayuka <basketball110512@gmail.com>
2025-02-20 12:57:49 +0900

    管理者権限の場合詳しくみるボタンを非表示にしていたが表示に変更（コメント削除機能で使用できるように）

--- src/resources/views/partials/shop-card.blade.php
+++ src/resources/views/partials/shop-card.blade.php
@@ -10,16 +10,16 @@
                 <p class="shop-card__area">#{{ $shop->area->area_name }}</p>
                 <p class="shop-card_genre">#{{ $shop->genre->genre_name }}</p>
             </div>
-            @if(!Request::is('admin/*'))
             <div class="shop-card__btn">
                 <button class="btn-submit" type="submit">詳しくみる</button>
+                @if(!Request::is('admin/*'))
                 <button>
                     <svg class="favorite-btn {{ in_array($shop->id, $favoriteShopIds) ? 'favorited' : '' }}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-id="{{ $shop->id }}">
                         <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
                     </svg>
                 </button>
+                @endif
             </div>
-            @endif
         </div>
     </form>
 </div>
